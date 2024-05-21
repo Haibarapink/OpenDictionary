f9ca49ec860841a7ef2a1142ebbf409cce50129b
pink0906 <12325996+pink0906@user.noreply.gitee.com>
2024-05-21 14:06:24 +0800

    更新函数handler

--- account/handler/handler.go
+++ account/handler/handler.go
@@ -1,20 +1,23 @@
 package handler
 
 import (
-	"net/http"
-
+	"github.com/Haibarapink/OpenDictionary/model"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
+	UserService model.UserService
 }
 
 type Config struct {
-	R *gin.Engine
+	R           *gin.Engine
+	UserService model.UserService
 }
 
 func NewHandler(c *Config) {
-	h := &Handler{}
+	h := &Handler{
+		UserService: c.UserService,
+	}
 	g := c.R.Group("/api/account")
 	g.GET("/me", h.Me)
 	g.POST("/signup", h.Signup)
@@ -24,52 +27,4 @@ func NewHandler(c *Config) {
 	g.POST("/image", h.Image)
 	g.DELETE("/image", h.DeleteImage)
 	g.PUT("/details", h.Details)
-}
-
-func (h *Handler) Me(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's me",
-	})
-}
-
-func (h *Handler) Signup(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's signup",
-	})
-}
-
-func (h *Handler) Signin(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's sigin",
-	})
-}
-
-func (h *Handler) Signout(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's signout",
-	})
-}
-
-func (h *Handler) Tokens(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's token",
-	})
-}
-
-func (h *Handler) Image(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's image",
-	})
-}
-
-func (h *Handler) DeleteImage(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's deleteImage",
-	})
-}
-
-func (h *Handler) Details(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's details",
-	})
 }
